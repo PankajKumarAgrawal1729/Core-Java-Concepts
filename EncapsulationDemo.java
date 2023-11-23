fb1c0115033a29029c815d6e89d0cae1d0337b36
Pankaj Kumar <pankaj.k@zohocorp.com>
2023-11-23 14:11:43 +0530

    update encapsulation

--- EncapsulationDemo.java
+++ EncapsulationDemo.java
@@ -1,24 +1,24 @@
 class Human {
 
-    //encapsulate the variable and only access by methods
+    // encapsulate the variable and only access by methods
     private int age;
     private String name;
 
-    //getter : methos who give the value
-    public int getAge(){
+    // getter : methos who give the value
+    public int getAge() {
         return age;
     }
 
-    //setter : method who set the value
-    public void setAge(int a){
+    // setter : method who set the value
+    public void setAge(int a) {
         age = a;
     }
 
-    public String getName(){
+    public String getName() {
         return name;
     }
 
-    public void setName(String n){
+    public void setName(String n) {
         name = n;
     }
 }
@@ -31,3 +31,8 @@ public class EncapsulationDemo {
         System.out.println(obj.getName() + " : " + obj.getAge());
     }
 }
+
+// Encapsulation in Java is a concept that combines data and the methods that
+// manipulate that data into a single unit, typically a class. It helps in
+// protecting the data from outside interference and misuse by restricting
+// direct access to it, promoting data integrity and security.
